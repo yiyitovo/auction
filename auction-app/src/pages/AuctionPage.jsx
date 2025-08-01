647f7906563607ef7c6d6756ce0724dfc40f1c30
yiyitovo <yiyitovo@outlook.com>
2025-08-01 01:44:49 +0100

    Update AuctionPage.jsx

--- auction-app/src/pages/AuctionPage.jsx
+++ auction-app/src/pages/AuctionPage.jsx
@@ -11,7 +11,7 @@ function AuctionPage() {
   const [type, setType] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/auctions')
+    axios.get('https://auction-zby2.onrender.com')
       .then(res => {
         const room = res.data.find(r => r.id === roomId);
         if (room) setType(room.type);
