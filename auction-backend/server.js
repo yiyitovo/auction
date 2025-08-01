ffbb67196f2ea11fef21edc607ae5790e3d51d83
yiyitovo <yiyitovo@outlook.com>
2025-08-01 11:25:25 +0100

    Update server.js

--- auction-backend/server.js
+++ auction-backend/server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
+    origin: ['http://localhost:5173', 'http://localhost:5175', 'https://auction-zby2.onrender.com'],
+    methods: ['GET', 'POST']
   }
 });
 
