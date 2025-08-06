74db81a6157f2f8c1ae2c95d2ce4f44e3936080c
王翠花 <yiyitovo@wangcuihuadeMacBook-Air.local>
2025-08-06 22:27:24 +0100

    Add MongoDB connection and user auth

--- auction-backend/server.js
+++ auction-backend/server.js
@@ -107,17 +107,3 @@ server.listen(3001, () => {
   console.log("Server running on http://localhost:3001");
 });
 
-// auctions/english.js
-module.exports = (io, socket, rooms) => {
-  socket.on("join-english", ({ roomId }) => {
-    socket.join(roomId);
-    socket.on("place-bid", ({ roomId, amount }) => {
-      const room = rooms[roomId];
-      if (!room || room.type !== "english") return;
-      if (!room.currentPrice || amount > room.currentPrice) {
-        room.currentPrice = amount;
-        io.to(roomId).emit("bid-update", { currentPrice: amount });
-      }
-    });
-  });
-};
