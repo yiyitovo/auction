64b67c8323efb7b2656620fcbba4c7cb6c860452
王翠花 <yiyitovo@wangcuihuadeMacBook-Air.local>
2025-08-15 22:30:26 +0100

    update: privacy & live feed

--- auction-app/src/App.jsx
+++ auction-app/src/App.jsx
@@ -1,29 +1,50 @@
+// src/App.jsx
 import { Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button, Container } from '@mui/material';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AuctionHall from './pages/AuctionHall';
-import BiddingInterface from './pages/BiddingInterface';
-import AuctionPage from './pages/AuctionPage';
 import CreateAuction from './pages/CreateAuction';
-import { AppBar, Toolbar, Button, Container } from '@mui/material';
+
+// 四个拍卖页
+import EnglishAuction from './pages/EnglishAuction';
+import DutchAuction from './pages/DutchAuction';
+import SealedAuction from './pages/SealedAuction';
+import DoubleAuction from './pages/DoubleAuction';
 
 function App() {
+  const isTeacher =
+    typeof window !== 'undefined' && localStorage.getItem('role') === 'teacher';
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <Button color="inherit" component={Link} to="/">Auction Hall</Button>
+          {isTeacher && (
+            <Button color="inherit" component={Link} to="/create">Create</Button>
+          )}
           <Button color="inherit" component={Link} to="/login">Login</Button>
           <Button color="inherit" component={Link} to="/register">Register</Button>
         </Toolbar>
       </AppBar>
-      <Container>
+
+      <Container sx={{ mt: 2 }}>
         <Routes>
           <Route path="/" element={<AuctionHall />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/create" element={<CreateAuction />} />  
           <Route path="/register" element={<Register />} />
-          <Route path="/auction/:id" element={<AuctionPage />} />
+          <Route path="/create" element={<CreateAuction />} />
+
+          {/* 按类型的房间路由 */}
+          <Route path="/english/:id" element={<EnglishAuction />} />
+          <Route path="/dutch/:id" element={<DutchAuction />} />
+          <Route path="/sealed/:id" element={<SealedAuction />} />
+          <Route path="/double/:id" element={<DoubleAuction />} />
+
+          {/* 兜底 */}
+          <Route path="*" element={<div style={{ padding: 16 }}>Page not found</div>} />
         </Routes>
       </Container>
     </>
@@ -31,4 +52,3 @@ function App() {
 }
 
 export default App;
-
